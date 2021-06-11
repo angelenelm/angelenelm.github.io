a7619b3963ef474ad0c8b5c14190d9f3c3d5f6e3
Angelene Manalo <angelenelmanalo@gmail.com>
2021-06-10 22:15:07 -0700

    dependencies

---
+++ build/sass.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+  return gulp.src('')
+})
