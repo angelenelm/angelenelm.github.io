a20b88c2d1237c3db87b3c00d1603c81701e16fa
Angelene Manalo <angelenelmanalo@gmail.com>
2021-06-11 22:35:20 -0700

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,38 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var cleanCSS = require('gulp-clean-css');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleancss = require('gulp-clean-css'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload(),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    watch = require('gulp-watch'),
+    cp = require('child_process'),
+    jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 
-gulp.task('sass', function() {
+/* run 'jekyll build' */
+gulp.task('jekyll-build', done => {
+    return cp.spawn(jekyll, ['build'], { stdio: 'inherit' }).on('close', done);
+})
+
+/* rebuild jekyll then reload the page */
+gulp.task('jekyll-rebuild', gulp.series(['jekyll-build', reload]))
+
+/* wait for jekyll-build, then launch server */
+gulp.task('serve', gulp.series('jekyll-build'), () => {
+  browserSync({
+    server: {
+      baseDir: '_site'
+    },
+    host: 'localhost'
+  })
+})
+
+gulp.task('sass', () => {
   return gulp
     .src('sass/**/*.scss')
+
+    .on('error', sass.logError)
 
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
@@ -18,3 +45,10 @@ gulp.task('sass', function() {
 
     .pipe(gulp.dest('css'))
 })
+
+gulp.task('watch', () => {
+  gulp.watch('scss/*.scss', gulp.series(['sass', reload]));
+  gulp.watch('sass/**/*.scss', gulp.series(['sass', reload]));
+})
+
+gulp.task('default', gulp.series('serve', 'sass', 'watch'));
